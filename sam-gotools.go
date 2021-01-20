be5618b7a386c018d565c22701f195d62ff3d293
Samad <samad.koushan@gmail.com>
2021-01-20 17:09:37 +0300

    Create sam-gotools.go

---
+++ sam-gotools.go
@@ -0,0 +1,17 @@
+package sam-gotools
+
+import (
+	"fmt"
+	"os"
+)
+
+func createDirIfNotExit(path string, perm os.FileMode) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, perm)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
